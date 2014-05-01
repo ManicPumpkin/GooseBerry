e5c74535af57362fc827f8f172118def6b979911
ManicPumpkin <danny.rubner@me.com>
2014-05-01 18:47:45 +0200

    formatting changes

--- GooseBerry/inc/GB_Mesh.h
+++ GooseBerry/inc/GB_Mesh.h
@@ -52,9 +52,9 @@ class GOOSEBERRY_API GB_Mesh
 						num_normals_,			//!< number of normals
 						num_tex_coords_;		//!< number of texture coordinates
 		
-		GB_Struct::Face *	faces_;				//!< list of faces
-		GB_Struct::Vertex *	vertices_;			//!< list of vetices
-		GB_Struct::Normal *	normals_;			//!< list of normals
+		GB_Struct::Face *		faces_;			//!< list of faces
+		GB_Struct::Vertex *		vertices_;		//!< list of vetices
+		GB_Struct::Normal *		normals_;		//!< list of normals
 		GB_Struct::TexCoord *	tex_coords_;	//!< list of texture coordiantes
 
 		//	(De-)Constructor
