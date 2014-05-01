49ae95bfaefcd954cd63f2b83ef510af9ff01052
ManicPumpkin <danny.rubner@me.com>
2014-05-01 19:05:26 +0200

    Black/White issue (2)

--- GooseBerryTestingApplication/src/GooseBerryTestingApplication.cc
+++ GooseBerryTestingApplication/src/GooseBerryTestingApplication.cc
@@ -168,6 +168,8 @@ GB_Enum::gbResult Run()
 	return GB_Enum::GB_OK;
 }
 
+float rotatef = 0.0f;
+
 GB_Enum::gbResult Render(float time)
 {
 	if (!g_render_init)
@@ -182,26 +184,19 @@ GB_Enum::gbResult Render(float time)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60, 1, 1, 10);
-	gluLookAt(0, 0, -2, 0, 0, 2, 0, 1, 0);
+	gluLookAt(0, 2, -4, 0, 0, 0, 0, 1, 0);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_TEXTURE_2D);
+	
+	if (rotatef > 360.0f)
+		rotatef = 0.0f;
 
-	glTranslatef(-0.5f, -0.5f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	/*
-	glBegin(GL_QUADS);
-	glTexCoord2d(0, 0); glVertex3f(1.0, 0.0, 0.0);
-	glTexCoord2d(0, 1); glVertex3f(1.0, 1.0, 0.0);
-	glTexCoord2d(1, 1); glVertex3f(0.0, 1.0, 0.0);	
-	glTexCoord2d(1, 0); glVertex3f(0.0, 0.0, 0.0);
-	glEnd();
-	*/
-
+	rotatef += 0.5f;
+	glRotatef(rotatef, 0.0f, 1.0f, 0.0f);
 	test_model->Draw();
 
 	glDisable(GL_TEXTURE_2D);
