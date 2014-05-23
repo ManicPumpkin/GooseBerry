5c8ff84bf5c39ade8c8ca9ce982a48a689adda8e
ManicPumpkin <danny.rubner@me.com>
2014-05-23 19:05:21 +0200

    # GB_Model no longer load BB3D if mesh isn't available

--- GooseBerry/src/GB_Model.cc
+++ GooseBerry/src/GB_Model.cc
@@ -58,12 +58,13 @@ GB_Model :: GB_Model(std::string mesh_file, GLuint texture_nr)
 	texture_		= new GB_Texture();
 	texture_nr_		= texture_nr;
 
-	GB_Loader::LoadMeshFile(mesh_file, mesh_);
-	GB_Loader::LoadMaterialFile(mesh_->msh_path_ + mesh_->mtl_lib_, material_);
- 	//GB_Loader::LoadTextureFile(material_->mat_path_ + material_->map_kd_, texture_, &texture_nr_);
+	has_mesh_		= (GB_OK != GB_Loader::LoadMeshFile(mesh_file, mesh_)) ? false : true;
+	has_material_	= (GB_OK != GB_Loader::LoadMaterialFile(mesh_->msh_path_ + mesh_->mtl_lib_, material_)) ? false : true;
+	//has_texture_	= (GB_OK != GB_Loader::LoadTextureFile(material_->mat_path_ + material_->map_kd_, texture_, &texture_nr_)) ? false : true;
 
 	// \todo add getter&setter for meshes
-	bb3d_			= new GB_BB3D(mesh_->GetVertices(), (int)mesh_->num_vertices_);
+	if (has_mesh_)
+		bb3d_		= new GB_BB3D(mesh_->GetVertices(), (int)mesh_->num_vertices_);
 }
 
 //==================================================================
@@ -76,10 +77,6 @@ void GB_Model :: Draw()
 {
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, texture_nr_);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	mesh_->Draw();
 	glDisable(GL_TEXTURE_2D);
 
